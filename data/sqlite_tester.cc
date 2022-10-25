28fb9dd966564b2bdd03aa79cab6444f90a732cd
nanason01 <nanason@umich.edu>
2022-10-25 13:03:03 -0400

    back to laptop

--- data/sqlite_tester.cc
+++ data/sqlite_tester.cc
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
+#include <tuple>
 
 using namespace std;
 
@@ -19,9 +21,38 @@ struct SqlError : std::exception {
     }
 };
 
+// Convert a db_str to a T
+template<typename T>
+static T db_digest(string db_str);
+
+template<>
+string db_digest(string db_str) {
+    return db_str;
+}
+
+template<typename T, typename... Ts>
+static tuple<T, Ts...> _digest_strings(char** p_flds) {
+    if constexpr (sizeof...(Ts) == 0)
+        return make_tuple(db_digest<T>(*p_flds));
+    else
+        return tuple_cat(tie(db_digest<T>(*p_flds)), _digest_strings<Ts...>(p_flds + 1));
+}
+
+template<typename... Ts>
+static tuple<Ts...> digest_strings(char** p_flds) {
+    if constexpr (sizeof...(Ts) == 0)
+        return tuple<>();
+    else
+        return _digest_strings<Ts...>(p_flds);
+}
+
+template<typename... Ts>
 static int fill_row(void* vec_ptr, int n_flds, char** p_flds, char** p_cols) {
-    reinterpret_cast<vector<Row>*>(vec_ptr)->push_back(
-        Row(p_flds, p_flds + n_flds)
+    // make sure we aren't accidentally dropping fields
+    assert(n_flds == sizeof...(Ts));
+
+    reinterpret_cast<vector<tuple<Ts...>>*>(vec_ptr)->push_back(
+        digest_strings<Ts...>(p_flds)
     );
 
     return 0;
@@ -37,8 +68,9 @@ public:
             throw DatabaseConnError();
     }
 
-    vector<Row> exec_sql(string sql) {
-        vector<Row> ret;
+    template<typename... Ts>
+    vector<tuple<Ts...>> exec_sql(string sql) {
+        vector<tuple<Ts...>> ret;
         char* err_msg;
 
         if (sqlite3_exec(db_conn, sql.c_str(), fill_row, &ret, &err_msg) != SQLITE_OK) {
@@ -57,12 +89,11 @@ public:
 int main() {
     DataConn dc("/Users/nick/Desktop/COMS/4156/CodeBuddiesASE/data/test_db");
 
-    const auto res = dc.exec_sql("SELECT * FROM Test;");
+    vector<tuple<string, string, string>> exp;
+    const auto res = dc.exec_sql<string, string, string>("SELECT * FROM Test;");
 
     for (const auto row : res) {
-        for (const auto item : row) {
-            cout << item << " ";
-        }
+        cout << get<0>(row) << " " << get<1>(row) << " " << get<2>(row);
         cout << "\n";
     }
 }
