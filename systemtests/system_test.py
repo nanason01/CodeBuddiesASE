0abe2dd2aa93ce3f6a11ad2a63a95d0a850a30d1
Urvee Ausekar <urveeausekar@gmail.com>
2022-11-23 09:46:30 -0500

    preliminary systemtests for all endpoints except get_annotated_trades and year_end_stats

--- systemtests/system_test.py
+++ systemtests/system_test.py
@@ -43,7 +43,6 @@ def test_refreshcredentials():
 
 def test_trade():
     global api_key
-
     trade = {
         "timestamp":"2/02/2020",
         "sold_currency":"BTC",
@@ -51,7 +50,45 @@ def test_trade():
         "sold_amount":"1",
         "bought_amount":"1"
     }
+    response = requests.post(SERVER + "trade", headers = {"Authorization" : "Bearer " + api_key}, json = trade)
+    assert response.status_code == 200
 
 
+def test_exchangekey():
+    global api_key
+    keydata = {
+        "exchange":"Kraken",
+        "readkey":"J9LYvgnqF6wG4H0Y7/Yr1ysdXke/O2vPdu58nQGp9bmK+e7R4OSyWAsU",
+        "secretkey":"VzP5UOZzOcsjJOw/9gU1D1QY78eBNO3LHnXIUngXxK7jbhy58EHpPOGI8b7CIg4D/304BOAwrxX5JwGVECJimg=="
+    }
+    response = requests.post(SERVER + "exchangekey", headers = {"Authorization" : "Bearer " + api_key}, json = keydata)
+    assert response.status_code == 200
 
+def test_removekey():
+    global api_key
+    keydata = { "exchange":"Kraken" }
+    response = requests.post(SERVER + "removekey", headers = {"Authorization" : "Bearer " + api_key}, json = keydata)
+    assert response.status_code == 200
+
+
+def test_tradepnl():
+    global api_key
+    trade = {
+        "timestamp":"2/02/2020",
+        "sold_currency":"BTC",
+        "bought_currency":"USD",
+        "sold_amount":"1",
+        "bought_amount":"1"
+    }
+    response = requests.post(SERVER + "trade_pnl", headers = {"Authorization" : "Bearer " + api_key}, json = trade)
+    assert response.status_code == 200
+    pnl = response.json()
+    assert float(pnl["pnl"])
+
+def test_portfolio_pnl():
+    global api_key
+    response = requests.get(SERVER + "portfolio_pnl", headers = {"Authorization" : "Bearer " + api_key})
+    assert response.status_code == 200
+    pnl = response.json()
+    assert float(pnl["pnl"])
 
