1064dc6a7f54ff4206d6550b018b110636d8ab7b
Urvee Ausekar <urveeausekar@gmail.com>
2022-11-25 17:10:09 -0500

    regrouped system tests for trade, exchangekey, removekey into separate classes and added fixtures

--- systemtests/system_test.py
+++ systemtests/system_test.py
@@ -53,26 +53,6 @@ class TestGetcredentialsClass:
         response = requests.get(SERVER, headers = {"Authorization" : "Bearer " + client_id + fake_api_key})
         assert response.status_code == 401
 
-    
-
-# def test_getcredentials():
-#     response = requests.get(SERVER + "getcredentials")
-
-#     assert response.status_code == 200
-
-#     authenticatedUser = response.json()
-
-#     global client_id
-#     global api_key
-#     global refresh_token
-#     client_id = authenticatedUser["client_id"]
-#     api_key = authenticatedUser["api_key"]
-#     refresh_token = authenticatedUser["refresh_token"]
-
-#     assert client_id == api_key[0 : 4] == refresh_token[0 : 4]
-
-#     assert requests.get(SERVER, headers = {"Authorization" : "Bearer " + api_key}).status_code == 200
-
         
 @pytest.fixture(scope="class")
 def endpoint_refreshcredentials(request):
@@ -135,44 +115,8 @@ class TestrefreshcredentialsClass:
 
 
 
-# def test_refreshcredentials():
-#     global client_id
-#     global api_key
-#     global refresh_token
-    
-#     response = requests.get(SERVER + "refreshcredentials", headers = {"Authorization" : "Bearer " + refresh_token})
-
-#     assert response.status_code == 200
-
-#     authenticatedUser = response.json()
-
-    
-#     api_key = authenticatedUser["api_key"]
-#     refresh_token = authenticatedUser["refresh_token"]
-
-#     assert client_id == authenticatedUser["client_id"] == api_key[0 : 4] == refresh_token[0 : 4]
-
-#     assert requests.get(SERVER, headers = {"Authorization" : "Bearer " + api_key}).status_code == 200
-
-#     # Check Invalid Creds
-#     if refresh_token == client_id + "aaaa":
-    #     fake_refresh_key = "aaab"
-    # else:
-    #     fake_refresh_key = "aaaa"
-    # response = requests.get(SERVER + "refreshcredentials", headers = {"Authorization" : "Bearer " + client_id + fake_refresh_key})
-    # assert response.status_code == 401
-
-    # # Check Invalid Client ID
-    # if client_id == "aaaa":
-    #     fake_client_id = "aaab"
-    # else:
-    #     fake_client_id = "aaaa"
-    # response = requests.get(SERVER + "refreshcredentials", headers = {"Authorization" : "Bearer " + fake_client_id + fake_refresh_key})
-    # assert response.status_code == 401
-
-
-def test_trade():
-    global api_key
+@pytest.fixture(scope="class")
+def endpoint_trade(request):
     trade = {
         "timestamp":"2/02/2020",
         "sold_currency":"BTC",
@@ -181,24 +125,67 @@ def test_trade():
         "bought_amount":"1"
     }
     response = requests.post(SERVER + "trade", headers = {"Authorization" : "Bearer " + api_key}, json = trade)
-    assert response.status_code == 200
+    request.cls.response = response
 
+@pytest.mark.usefixtures("endpoint_trade")
+class TestTradeClass:
+    def test_response_trade(self):
+        assert self.response.status_code == 200
 
-def test_exchangekey():
-    global api_key
+# def test_trade():
+#     global api_key
+#     trade = {
+#         "timestamp":"2/02/2020",
+#         "sold_currency":"BTC",
+#         "bought_currency":"USD",
+#         "sold_amount":"1",
+#         "bought_amount":"1"
+#     }
+#     response = requests.post(SERVER + "trade", headers = {"Authorization" : "Bearer " + api_key}, json = trade)
+#     assert response.status_code == 200
+
+        
+@pytest.fixture(scope="class")
+def endpoint_exchangekey(request):
     keydata = {
         "exchange":"Kraken",
         "readkey":"J9LYvgnqF6wG4H0Y7/Yr1ysdXke/O2vPdu58nQGp9bmK+e7R4OSyWAsU",
         "secretkey":"VzP5UOZzOcsjJOw/9gU1D1QY78eBNO3LHnXIUngXxK7jbhy58EHpPOGI8b7CIg4D/304BOAwrxX5JwGVECJimg=="
     }
     response = requests.post(SERVER + "exchangekey", headers = {"Authorization" : "Bearer " + api_key}, json = keydata)
-    assert response.status_code == 200
+    request.cls.response = response
 
-def test_removekey():
-    global api_key
+@pytest.mark.usefixtures("endpoint_exchangekey")
+class TestExchangekeyClass:
+    def test_response_exchangekey(self):
+        assert self.response.status_code == 200
+
+# def test_exchangekey():
+#     global api_key
+#     keydata = {
+#         "exchange":"Kraken",
+#         "readkey":"J9LYvgnqF6wG4H0Y7/Yr1ysdXke/O2vPdu58nQGp9bmK+e7R4OSyWAsU",
+#         "secretkey":"VzP5UOZzOcsjJOw/9gU1D1QY78eBNO3LHnXIUngXxK7jbhy58EHpPOGI8b7CIg4D/304BOAwrxX5JwGVECJimg=="
+#     }
+#     response = requests.post(SERVER + "exchangekey", headers = {"Authorization" : "Bearer " + api_key}, json = keydata)
+#     assert response.status_code == 200
+        
+@pytest.fixture(scope="class")
+def endpoint_removekey(request):
     keydata = { "exchange":"Kraken" }
     response = requests.post(SERVER + "removekey", headers = {"Authorization" : "Bearer " + api_key}, json = keydata)
-    assert response.status_code == 200
+    request.cls.response = response
+
+@pytest.mark.usefixtures("endpoint_removekey")
+class TestRemovekeyClass:
+    def test_response_removekey(self):
+        assert self.response.status_code == 200
+
+# def test_removekey():
+#     global api_key
+#     keydata = { "exchange":"Kraken" }
+#     response = requests.post(SERVER + "removekey", headers = {"Authorization" : "Bearer " + api_key}, json = keydata)
+#     assert response.status_code == 200
 
 
 def test_tradepnl():
