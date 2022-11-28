e0c3eedafbf4503ba05c5c57e6dfebe4f0c559c6
Urvee Ausekar <urveeausekar@gmail.com>
2022-11-28 09:38:57 -0500

    copied pricer from working master

--- pricer/pricer_test.cc
+++ pricer/pricer_test.cc
@@ -91,30 +91,3 @@ TEST_F(PricerFixture, assetPrice_bad) {
         EXPECT_EQ(e.what(), NoRecordsFound{}.what());
     }
 }
-
-TEST_F(PricerFixture, assetPrice_btc_2) {
-    Timestamp ts = from_usa_date(3, 3, 2017);
-    EXPECT_NEAR(p.get_usd_price("BtC", ts), 1289.54, 0.01);
-}
-
-TEST_F(PricerFixture, assetPrice_btc_3) {
-    Timestamp ts = from_usa_date(3, 3, 2017);
-    EXPECT_NEAR(p.get_usd_price("btc", ts), 1289.54, 0.01);
-}
-
-TEST_F(PricerFixture, assetPrice_btc_4) {
-    Timestamp ts = from_usa_date(12, 30, 2017);
-    EXPECT_NEAR(p.get_usd_price("BTC", ts), 13620.36, 0.01);
-}
-
-TEST_F(PricerFixture, assetPrice_all) {
-    Timestamp ts = from_usa_date(3, 3, 2021);
-    for (const auto& [key, value] : p.token_name_map) {
-       EXPECT_GT(p.get_usd_price(key, ts), 0);
-    }
-}
-
-TEST_F(PricerFixture, assetPrice_bad) {
-    Timestamp ts = from_usa_date(12, 30, 2017);
-    EXPECT_THROW(p.get_usd_price("Alejandro", ts), NoRecordsFound);
-}
