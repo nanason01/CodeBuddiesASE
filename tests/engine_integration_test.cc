9e1d850c1ced40b360a320bdbc7cd0a01f5d8f59
nanason01 <65088304+nanason01@users.noreply.github.com>
2022-11-28 23:13:11 -0500

    Nick integration tests (#42)
    * engine tests mostly done, pricer is very flaky
    
    * data integration test
    
    * fix small style error
    
    * fix bug, revert anomolous change

--- tests/engine_integration_test.cc
+++ tests/engine_integration_test.cc
@@ -26,6 +26,27 @@ const std::vector<Trade> sample_trades = {
         0.5,
         13.514,
     },
+    {
+        from_usa_date(5, 14, 2021),
+        "ETH",
+        "BTC",
+        12.821,
+        1.0,
+    },
+    {
+        from_usa_date(6, 2, 2021),
+        "BTC",
+        "ETH",
+        0.5,
+        7.813,
+    },
+    {
+        from_usa_date(5, 20, 2022),
+        "BTC",
+        "ETH",
+        1.0,
+        14.925,
+    },
 };
 
 // so the expected output shouldn't change if you run on different dates
@@ -37,12 +58,77 @@ TEST(EngineIntegration, GetMatchedTrades) {
 
     try {
         res = m.get_matched_trades(sample_trades);
-    } catch (std::exception const &e) {
-        EXPECT_EQ(e.what(), RateLimitedQuery{}.what());
+    } catch (const RateLimitedQuery& rq) {
+        std::cerr << "rate limited, redo later\n";
+        return;
     }
 
     for (const auto& mt : res)
         std::cout << mt << std::endl;
+    EXPECT_EQ(res.size(), 9);
+
+    for (const auto& mt : res) {
+        switch (mt.term) {
+        case Term::UnmatchedSell:
+            EXPECT_EQ(mt.sold_timestamp, from_usa_date(8, 10, 2018));
+            EXPECT_EQ(mt.currency, "ETH");
+            EXPECT_NEAR(mt.sz, 19.608, 0.1);
+            EXPECT_NEAR(mt.pnl, 7131.14, 0.1);
+            break;
+        case Term::Held:
+            if (mt.bought_timestamp == from_usa_date(9, 11, 2020)) {
+                EXPECT_EQ(mt.currency, "ETH");
+                EXPECT_NEAR(mt.sz, 0.69, 0.1);
+                EXPECT_NEAR(mt.pnl, 556.11, 0.1);
+            } else if (mt.bought_timestamp == from_usa_date(6, 2, 2021)) {
+                EXPECT_EQ(mt.currency, "ETH");
+                EXPECT_NEAR(mt.sz, 7.81, 0.1);
+                EXPECT_NEAR(mt.pnl, -11426.8, 0.1);
+            } else if (mt.bought_timestamp == from_usa_date(5, 20, 2022)) {
+                EXPECT_EQ(mt.currency, "ETH");
+                EXPECT_NEAR(mt.sz, 14.925, 0.1);
+                EXPECT_NEAR(mt.pnl, -12742, 0.1);
+            } else {
+                ADD_FAILURE();
+            }
+            break;
+        case Term::Short:
+            if (mt.bought_timestamp == from_usa_date(9, 11, 2020)) {
+                EXPECT_EQ(mt.sold_timestamp, from_usa_date(5, 14, 2021));
+                EXPECT_EQ(mt.currency, "ETH");
+                EXPECT_NEAR(mt.sz, 12.82, 0.1);
+                EXPECT_NEAR(mt.pnl, 43369.6, 0.1);
+            } else if (mt.bought_timestamp == from_usa_date(5, 14, 2021)) {
+                EXPECT_EQ(mt.sold_timestamp, from_usa_date(6, 2, 2021));
+                EXPECT_EQ(mt.currency, "BTC");
+                EXPECT_NEAR(mt.sz, 0.5, 0.1);
+                EXPECT_NEAR(mt.pnl, -6646.36, 0.1);
+            } else {
+                ADD_FAILURE();
+            }
+            break;
+        case Term::Long:
+            if (mt.bought_timestamp == from_usa_date(5, 14, 2021)) {
+                EXPECT_EQ(mt.sold_timestamp, from_usa_date(5, 20, 2022));
+                EXPECT_EQ(mt.currency, "BTC");
+                EXPECT_NEAR(mt.sz, 0.5, 0.1);
+                EXPECT_NEAR(mt.pnl, -9795.19, 0.1);
+            } else if (mt.sold_timestamp == from_usa_date(9, 11, 2020)) {
+                EXPECT_EQ(mt.bought_timestamp, from_usa_date(8, 10, 2018));
+                EXPECT_EQ(mt.currency, "BTC");
+                EXPECT_NEAR(mt.sz, 0.5, 0.1);
+                EXPECT_NEAR(mt.pnl, 1905.45, 0.1);
+            } else if (mt.sold_timestamp == from_usa_date(5, 20, 2022)) {
+                EXPECT_EQ(mt.bought_timestamp, from_usa_date(8, 10, 2018));
+                EXPECT_EQ(mt.currency, "BTC");
+                EXPECT_NEAR(mt.sz, 0.5, 0.1);
+                EXPECT_NEAR(mt.pnl, 11925.6, 0.1);
+            } else {
+                ADD_FAILURE();
+            }
+            break;
+        }
+    }
 }
 
 TEST(EngineIntegration, GetPNLFrom) {
@@ -50,12 +136,15 @@ TEST(EngineIntegration, GetPNLFrom) {
     PNL res1, res2;
 
     try {
-        res1 = m.get_pnl_from(sample_trades[ 0 ], sample_end);
-        res2 = m.get_pnl_from(sample_trades[ 1 ], sample_end);
-        std::cout << "res1: " << res1 << " res2: " << res2 << std::endl;
-    } catch (std::exception const &e) {
-        EXPECT_EQ(e.what(), RateLimitedQuery{}.what());
+        res1 = m.get_pnl_from(sample_trades[ 2 ], sample_end);
+        res2 = m.get_pnl_from(sample_trades[ 3 ], sample_end);
+    } catch (const RateLimitedQuery& rq) {
+        std::cerr << "rate limited, redo later\n";
+        return;
     }
+
+    EXPECT_NEAR(res1, 1310.21, 0.1);
+    EXPECT_NEAR(res2, 1030.4, 0.1);
 }
 
 TEST(EngineIntegration, GetNetPNL) {
@@ -64,10 +153,12 @@ TEST(EngineIntegration, GetNetPNL) {
 
     try {
         res = m.get_net_pnl(sample_trades, sample_end);
-        std::cout << "res: " << res << std::endl;
-    } catch (std::exception const &e) {
-        EXPECT_EQ(e.what(), RateLimitedQuery{}.what());
+    } catch (const RateLimitedQuery& rq) {
+        std::cerr << "rate limited, redo later\n";
+        return;
     }
+
+    EXPECT_NEAR(res, 4594.44, 0.1);
 }
 
 TEST(EngineIntegration, GetYearEndPNL) {
@@ -76,10 +167,13 @@ TEST(EngineIntegration, GetYearEndPNL) {
 
     try {
         res = m.get_year_end_pnl(sample_trades, sample_end);
-        std::cout << "actual: " << res.actual << " lt: " << res.lt_realized << " st: " << res.st_realized << std::endl;
-    } catch (std::exception const &e) {
+    } catch (std::exception const& e) {
         EXPECT_EQ(e.what(), RateLimitedQuery{}.what());
     }
+
+    EXPECT_NEAR(res.actual, 0.0, 0.1);
+    EXPECT_NEAR(res.lt_realized, 0.0, 0.1);
+    EXPECT_NEAR(res.st_realized, 0.0, 0.1);
 }
 
 TEST(EngineIntegration, GetPNLSnapshots) {
@@ -88,13 +182,12 @@ TEST(EngineIntegration, GetPNLSnapshots) {
 
     try {
         res = m.get_pnl_snapshots(sample_trades, sample_end);
-    } catch (std::exception const &e) {
-        EXPECT_EQ(e.what(), RateLimitedQuery{}.what());
+    } catch (const RateLimitedQuery& rq) {
+        std::cerr << "rate limited, redo later\n";
+        return;
     }
 
-    for (const auto& snapshot : res) {
-        std::cout << "pnl: " << snapshot.pnl << " ts: " << snapshot.timestamp << std::endl;
-    }
+    SUCCEED();
 }
 
 TEST(EngineIntegration, GetEarliestLongTermSells) {
@@ -103,11 +196,10 @@ TEST(EngineIntegration, GetEarliestLongTermSells) {
 
     try {
         res = m.get_earliest_long_term_sells(sample_trades, sample_end);
-    } catch (std::exception const &e) {
-        EXPECT_EQ(e.what(), RateLimitedQuery{}.what());
+    } catch (const RateLimitedQuery& rq) {
+        std::cerr << "rate limited, redo later\n";
+        return;
     }
 
-    for (const auto& lt_sell : res) {
-        std::cout << lt_sell << std::endl;
-    }
+    SUCCEED();
 }
